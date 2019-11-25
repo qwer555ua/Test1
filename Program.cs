93fed19f3b9adb6da905317af358ac05d072e670
qwer555ua <58169138+qwer555ua@users.noreply.github.com>
2019-11-25 12:15:36 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _TestOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var s = Console.ReadLine();
+            s=s.ToLower();
+            s=s.Replace(" ", "");
+
+            Console.WriteLine("Hello, user "+s);
+        }
+    }
+}
